3469ad9c2695bd10baab4bd595fef81e0928ea16
zubayrrr <abdulfaiz076@gmail.com>
2020-05-21 17:41:51 -0700

    fix: renamed projects to works

---
+++ src/pages/works.js
@@ -0,0 +1,97 @@
+import React from "react"
+import Helmet from 'react-helmet';
+import { graphql } from "gatsby"
+
+import Repository from "../components/repository"
+import Layout from "../components/layout"
+import Avatar from "../components/avatar"
+
+
+const Works = ({ data }) => {
+  const {
+    name,
+    avatarUrl,
+    isHireable,
+    repositories,
+  } = data.githubData.viewer
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Works</title>
+      </Helmet>
+     <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 800,
+          paddingTop: 0,
+        }}
+      >
+        <div
+          style={{
+            display: `flex`,
+            alignItems: `center`,
+            margin: `1.45rem 0`,
+          }}
+        >
+          <Avatar img={avatarUrl} />
+          <div style={{ padding: 16 }}>
+            <h2 style={{ border: `none` }}>{name}</h2>
+            {isHireable && (
+              <h3 style={{ marginBottom: 0 }}>
+                I'm Hireable{" "}
+                <span role="img" aria-label="hand">
+                  😎
+                </span>
+              </h3>
+            )}
+          </div>
+        </div>
+        {repositories.nodes
+          .map(repo => <Repository key={repo.name} repo={repo} />)
+          .reverse()}
+      </div>
+      <p style={{textAlign: `center`}}><a target="_blank" rel="noopener noreferrer" href="https://github.com/zubayrrr?tab=repositories">More on Github</a></p>
+    </Layout>
+  )
+}
+
+export default Works
+
+export const gitHubQuery = graphql`
+{
+  githubData {
+    viewer {
+      name
+      avatarUrl
+      isHireable
+      resourcePath
+      repositories(last: 12, privacy: PUBLIC, orderBy: { field: STARGAZERS, direction: ASC } ) {
+        nodes {
+          name
+          description
+          homepageUrl
+          forkCount
+          createdAt
+          updatedAt
+          resourcePath
+          languages(last: 1, orderBy: { field: SIZE, direction: ASC } ) {
+            edges {
+              node {
+                name
+                color
+              }
+            }
+          }
+          licenseInfo {
+            name
+          }
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
+`
